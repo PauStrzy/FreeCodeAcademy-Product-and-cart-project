621dbf56d7911cb4d186b59606257290fa0b1c55
Paulina <paustrzy@gmail.com>
2023-01-05 19:37:45 +0100

    Created project with FreeCodeAcademy - Product and cart

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PastOrders from '../views/PastOrders.vue'
+import ProductsView from '../views/ProductsView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+	{
+		path: '/',
+		name: 'home',
+		component: HomeView,
+	},
+	{
+		path: '/products',
+		name: 'products',
+		component: ProductsView,
+	},
+	{
+		path: '/past-orders',
+		name: 'PastOrders',
+		component: PastOrders,
+	},
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
 })
 
 export default router
